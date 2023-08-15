fc7055252820d1b35b83312e20bdd9b833908e21
Nazarov Dmitry <Difat1@inbox.ru>
2023-08-15 19:11:01 +0300

    add method main

--- src/Main.java
+++ src/Main.java
@@ -6,48 +6,40 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        File main = new File("src/main");
-        File utils = new File("src/main");
-        File test = new File("src/test");
-        File drawables = new File("res/drawables");
-        File vectors = new File("res/vectors");
-        File icons = new File("res/icons");
-        File savegames = new File("savegames");
-        File tempTxt = new File("temp.txt");
-        File mainJava = new File("src/main/Main.java");
-        File utilsJava = new File("src/main/Utils.java");
-        List<File> listFolder = new ArrayList<>(List.of(new File[]{main, utils, test, drawables, vectors, icons, savegames}));
-        List<File> listFile = new ArrayList<>(List.of(new File[]{mainJava, utilsJava}));
+        List<String> listFolder = new ArrayList<>(List.of(new String[]{"src/main", "src/main", "src/test", "res/drawables", "res/vectors", "res/icons", "savegames"}));
+        List<String> listFile = new ArrayList<>(List.of(new String[]{"temp.txt", "src/main/Main.java", "src/main/Utils.java"}));
         StringBuilder temp = new StringBuilder();
 
-        try {
-            tempTxt.createNewFile();
-            temp.append("фаил " + tempTxt.getName() + " успешно создан \n");
-        } catch (IOException e) {
-            temp.append("произошла ошибка при создание файла " + tempTxt.getName() + "\n");
-            throw new RuntimeException(e);
-        }
+        createFolder(listFolder, temp);
+        createFiles(listFile, temp);
+        logTemp(temp);    }
 
-
-        for (File folder : listFolder) {
-            folder.mkdirs();
-            temp.append("дериктория " + folder.getName() + " успешно создана \n");
-        }
-
-        for (File file : listFile) {
-            try {
-                file.createNewFile();
-                temp.append("фаил " + file.getName() + " успешно создан \n");
-            } catch (IOException e) {
-                temp.append("произошла ошибка при создание файла " + file.getName() + "\n");
-                throw new RuntimeException(e);
-            }
-        }
-
+    private static void logTemp(StringBuilder temp) {
         try (FileWriter writer = new FileWriter("temp.txt", false)) {
             writer.write(String.valueOf(temp));
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void createFiles(List<String> listFile, StringBuilder temp) {
+        for (String fileName : listFile) {
+            try {
+                File file = new File(fileName);
+                file.createNewFile();
+                temp.append("фаил " + file.getName() + " успешно создан \n");
+            } catch (IOException e) {
+                temp.append("произошла ошибка при создание файла " + fileName + "\n");
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void createFolder(List<String> listFolder, StringBuilder temp) {
+        for (String folderPath : listFolder) {
+            File folder = new File(folderPath);
+            folder.mkdirs();
+            temp.append("дериктория " + folder.getName() + " успешно создана \n");
         }
     }
 }
